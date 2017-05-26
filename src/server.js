0fe9f44ed627a7ee67b15b90a3489d6e6731d2e5
malikoum94 <mmazouz@student.42.fr>
2017-05-26 17:20:54 +0200

    debug pub

--- src/server.js
+++ src/server.js
@@ -53,14 +53,14 @@ if (!process.env.REQUEST_TOKEN.length) {
       const result = await getFireNumber('+33761391453')
       // await updateFireBot({name: 'bot', type: 'facebook-fr', access: '6098592f-e819-40a4-96f5-dc4f17b85583'}, 'vm-c642f6f5-bfc8-4b7e-809b-9020d974b0be')
       // await updateFireBot2('vm-c642f6f5-bfc8-4b7e-809b-9020d974b0be', 'facebook', 0)
-      const service = await getFireBot('vm-c642f6f5-bfc8-4b7e-809b-9020d974b0be', '1696871486992920')
-      console.log(service)
+      // const service = await getFireBot('vm-c642f6f5-bfc8-4b7e-809b-9020d974b0be', '1696871486992920')
+      // console.log(service)
       console.log(result)
     } catch (e) {
       console.log(e)
     }
     deleteConv('1696871486992920')
-    deleteConv('U5CJZS8V9')
+    deleteConv('U5K7K9E6A')
     console.log('Our bot is running on port', app.get('port'))
   })
 }
